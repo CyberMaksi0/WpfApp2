394dddc60eac9312287e164223eced85cd5f154f
glowacki.maksymilian <glowacki.maksymilian@STACJA105-10>
2023-10-18 13:06:47 +0200

    Dodaj pliki projektów.

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows;
+
+namespace AnagramChecker
+{
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private bool isFirstWordValid = true;
+        private bool isSecondWordValid = true;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public bool IsFirstWordValid
+        {
+            get { return isFirstWordValid; }
+            set
+            {
+                isFirstWordValid = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("IsFirstWordValid"));
+            }
+        }
+
+        public bool IsSecondWordValid
+        {
+            get { return isSecondWordValid; }
+            set
+            {
+                isSecondWordValid = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("IsSecondWordValid"));
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private void CheckAnagrams_Click(object sender, RoutedEventArgs e)
+        {
+            string firstWord = firstWordTextBox.Text;
+            string secondWord = secondWordTextBox.Text;
+
+            IsFirstWordValid = !string.IsNullOrWhiteSpace(firstWord);
+            IsSecondWordValid = !string.IsNullOrWhiteSpace(secondWord);
+
+            if (IsFirstWordValid && IsSecondWordValid)
+            {
+                string sortedFirstWord = new string(firstWord.ToCharArray().OrderBy(c => c).ToArray());
+                string sortedSecondWord = new string(secondWord.ToCharArray().OrderBy(c => c).ToArray());
+
+                if (sortedFirstWord.Equals(sortedSecondWord, StringComparison.OrdinalIgnoreCase))
+                {
+                    resultTextBlock.Text = "Słowa są anagramami!";
+                }
+                else
+                {
+                    resultTextBlock.Text = "Słowa nie są anagramami.";
+                }
+            }
+            else
+            {
+                resultTextBlock.Text = "Wprowadź oba słowa, aby sprawdzić, czy są anagramami.";
+            }
+        }
+    }
+}
