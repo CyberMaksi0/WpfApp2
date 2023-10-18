276860670508ca0be64e5b80820c12ec8e9edf15
CyberMaksi0 <113881940+CyberMaksi0@users.noreply.github.com>
2023-10-18 13:27:49 +0200

    Update MainWindow.xaml.cs

--- WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/MainWindow.xaml.cs
@@ -46,9 +46,69 @@ namespace AnagramChecker
             IsFirstWordValid = !string.IsNullOrWhiteSpace(firstWord);
             IsSecondWordValid = !string.IsNullOrWhiteSpace(secondWord);
 
+            if (IsFirstWordValid && IsSecondWordValid)
+            {
+                string sortedFirstWord = new string(firstWord.ToLower().ToCharArray().OrderBy(c => c).ToArray());
+                string sortedSecondWord = new string(secondWord.ToLower().ToCharArray().OrderBy(c => c).ToArray());
+
+                if (sortedFirstWord.Equals(sortedSecondWord, StringComparison.Ordinal))
+                {
+                    resultTextBlock.Text = "Słowa są anagramami!";
+                }
+                else
+                {
+                    resultTextBlock.Text = "Słowa nie są anagramami.";
+                }
+            }
+            else
+            {
+                resultTextBlock.Text = "Wprowadź oba słowa, aby sprawdzić, czy są anagramami.";
+            }
         }
 
-        private void CheckAnagrams_Click1(object sender, RoutedEventArgs e)
+    }
+}
+using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows;
+
+namespace AnagramChecker
+{
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private bool isFirstWordValid = true;
+        private bool isSecondWordValid = true;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public bool IsFirstWordValid
+        {
+            get { return isFirstWordValid; }
+            set
+            {
+                isFirstWordValid = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("IsFirstWordValid"));
+            }
+        }
+
+        public bool IsSecondWordValid
+        {
+            get { return isSecondWordValid; }
+            set
+            {
+                isSecondWordValid = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("IsSecondWordValid"));
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private void CheckAnagrams_Click(object sender, RoutedEventArgs e)
         {
             string firstWord = firstWordTextBox.Text;
             string secondWord = secondWordTextBox.Text;
@@ -75,5 +135,6 @@ namespace AnagramChecker
                 resultTextBlock.Text = "Wprowadź oba słowa, aby sprawdzić, czy są anagramami.";
             }
         }
+
     }
 }
